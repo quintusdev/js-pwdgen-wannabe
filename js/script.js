2ac515b4c316d55fb232b3ded1681044c80ed24b
quintusdev <quintusdev92@gmail.com>
2023-05-16 15:17:23 +0200

    ES. COMPLETATO

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//INSERIMENTO DATI UTENTE.
+
+//INSERIMENTO NOME UTENTE.
+
+let nome = prompt('Inserisci il tuo nome.');
+
+//INSERIMENTO COGNOME UTENTE.
+
+let cognome = prompt('Inserisci il tuo cognome.');
+
+//INSERIMENTO COLORE PREFERITO UTENTE
+
+let colore = prompt('Inserisci il tuo colore preferito.');
+
+//FINE INSERIMENTO DATI UTENTE
+
+//CONCATENAZIONE DEGLI INPUT E VISUALIZZAZIONE A SCHERMO.
+
+let password = (nome+cognome+colore+21);
+alert(password);
